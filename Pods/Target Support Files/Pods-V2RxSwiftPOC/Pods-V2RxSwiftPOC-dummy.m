b3a3bff3b323257a011f71a994a6d7d6145927f4
Aanchal Nigam <aanchal.nigam@v2solutions.com>
2016-05-09 15:41:59 +0530

    Added Pods to Project

---
+++ Pods/Target Support Files/Pods-V2RxSwiftPOC/Pods-V2RxSwiftPOC-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_V2RxSwiftPOC : NSObject
+@end
+@implementation PodsDummy_Pods_V2RxSwiftPOC
+@end
---
+++ Pods/Target Support Files/Pods-V2RxSwiftPOC/Pods-V2RxSwiftPOC-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_V2RxSwiftPOCVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_V2RxSwiftPOCVersionString[];
+
